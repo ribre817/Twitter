67284ad31c95aab8497ff662d6f5e0d9c4d7ed39
Rian Walker <rw1021@msstate.edu>
2016-02-20 21:42:23 -0600

    Revert "Final commit."
    This reverts commit 2755db8b3d62052561ceaaec708e2b613c9b0935.

--- Twitter/ViewController.swift
+++ Twitter/ViewController.swift
@@ -23,17 +23,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onLogin(sender: AnyObject) {
-        
-        TwitterClient.sharedInstance.loginWithCompletion(){
-            (user: User?, error: NSError?) in
-            if user != nil {
-                print("I've logged in.")
-                self.performSegueWithIdentifier("loginSegue", sender: <#T##AnyObject?#>)
-            } else {
-                // handle login error
-                print(error)
-            }
+        TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
+        TwitterClient.sharedInstance.fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "cptwitterRian://oauth"), scope: nil,
+            success: { (requestToken: BDBOAuth1Credential!) -> Void in
+            print("Got the request token.")
+            var authURL = NSURL(string: "https://api.twitter.com/oauth/authorize?oauth_token=\(requestToken.token)")
+            UIApplication.sharedApplication().openURL(authURL!)
+            }) { (error: NSError!) -> Void in
+                print("Failed to get request token: \(error)")
         }
     }
+
 }
 
